27fc68dfab01b6ad4836983c8e336638c3fcf1f0
Joseph <forkjoseph@users.noreply.github.com>
2016-02-10 19:02:13 -0500

    Update battery_remain.sh
    removed debugging print

--- scripts/battery_remain.sh
+++ scripts/battery_remain.sh
@@ -20,18 +20,17 @@ pmset_battery_remaining_time() {
 print_battery_remain() {
 	if command_exists "pmset"; then
 		pmset_battery_remaining_time
-    echo "HELLL"
 	elif command_exists "upower"; then
 		battery=$(upower -e | grep battery | head -1)
-    if is_chrome; then
-      if battery_discharging; then
-        upower -i $battery | grep 'time to empty' | awk '{printf "- %s %s left", $4, $5}'
-      else
-        upower -i $battery | grep 'time to full' | awk '{printf "- %s %s till full", $4, $5}'
-      fi
-    else
-      upower -i $battery | grep remain | awk '{print $4}'
-    fi
+		if is_chrome; then
+			if battery_discharging; then
+        			upower -i $battery | grep 'time to empty' | awk '{printf "- %s %s left", $4, $5}'
+      			else
+        			upower -i $battery | grep 'time to full' | awk '{printf "- %s %s till full", $4, $5}'
+      			fi
+    		else
+      			upower -i $battery | grep remain | awk '{print $4}'
+    		fi
 	elif command_exists "acpi"; then
 		acpi -b | grep -Eo "[0-9]+:[0-9]+:[0-9]+"
 	fi
