c23ce2c8523e53bc58cb8187056072c07d4c6c3f
Mikael Göransson <github@mgor.se>
2016-11-25 12:22:00 +0100

    only show status when AC is offline
    configurable with @batt_toggle_plug_status in .tmux.conf

--- scripts/battery_remain.sh
+++ scripts/battery_remain.sh
@@ -20,12 +20,12 @@ print_battery_remain() {
 		battery=$(upower -e | grep battery | head -1)
 		if is_chrome; then
 			if battery_discharging; then
-				upower -i $battery | grep 'time to empty' | awk '{printf "- %s %s left", $4, $5}'
+				upower -i "$battery" | grep 'time to empty' | awk '{printf "- %s %s left", $4, $5}'
 			else
-				upower -i $battery | grep 'time to full' | awk '{printf "- %s %s till full", $4, $5}'
+				upower -i "$battery" | grep 'time to full' | awk '{printf "- %s %s till full", $4, $5}'
 			fi
 		else
-			upower -i $battery | grep -E '(remain|time to empty)' | awk '{print $(NF-1)}'
+			upower -i "$battery" | grep -E '(remain|time to empty)' | awk '{print $(NF-1)}'
 		fi
 	elif command_exists "acpi"; then
 		acpi -b | grep -Eo "[0-9]+:[0-9]+:[0-9]+"
@@ -35,11 +35,13 @@ print_battery_remain() {
 print_battery_full() {
 	if command_exists "upower"; then
 		battery=$(upower -e | grep battery | head -1)
-		upower -i $battery | grep 'time to full' | awk '{printf "- %s %s till full", $4, $5}'
+		upower -i "$battery" | grep 'time to full' | awk '{printf "- %s %s till full", $4, $5}'
 	fi
 }
 
 main() {
+	ac_online && { echo ""; return 0; }
+
 	if battery_discharging; then
 		print_battery_remain
 	else
