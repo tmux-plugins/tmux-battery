51499f1cc7dea20043e29318a0a55737aef709c9
Philipp Schmitt <pschmitt@users.noreply.github.com>
2017-08-22 11:40:34 +0200

    Fix multiple batteries on linux (#57)

--- scripts/battery_percentage.sh
+++ scripts/battery_percentage.sh
@@ -9,19 +9,19 @@ print_battery_percentage() {
 	if command_exists "pmset"; then
 		pmset -g batt | grep -o "[0-9]\{1,3\}%"
 	elif command_exists "upower"; then
-		local batteries=( $(upower -e | grep battery) )
-		if [ -z "$batteries" ]; then
+		local battery=$(upower -e | grep -m 1 battery)
+		if [ -z "$battery" ]; then
 			return
 		fi
 		local energy
 		local energy_full
-		for battery in ${batteries[@]}; do
-			energy=$(upower -i $battery | awk -v nrg="$energy" '/energy:/ {print nrg+$2}')
-			energy_full=$(upower -i $battery | awk -v nrgfull="$energy_full" '/energy-full:/ {print nrgfull+$2}')
-		done
-		echo $energy $energy_full | awk '{printf("%d%%", ($1/$2)*100)}'
+		energy=$(upower -i $battery | awk -v nrg="$energy" '/energy:/ {print nrg+$2}')
+		energy_full=$(upower -i $battery | awk -v nrgfull="$energy_full" '/energy-full:/ {print nrgfull+$2}')
+		if [ -n "$energy" ] && [ -n "$energy_full" ]; then
+			echo $energy $energy_full | awk '{printf("%d%%", ($1/$2)*100)}'
+		fi
 	elif command_exists "acpi"; then
-		acpi -b | grep -Eo "[0-9]+%"
+		acpi -b | grep -m 1 -Eo "[0-9]+%"
 	fi
 }
 
--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -31,11 +31,10 @@ battery_status() {
 	if command_exists "pmset"; then
 		pmset -g batt | awk -F '; *' 'NR==2 { print $2 }'
 	elif command_exists "upower"; then
-		# sort order: attached, charging, discharging
-		for battery in $(upower -e | grep battery); do
-			upower -i $battery | grep state | awk '{print $2}'
-		done | sort | head -1
+		local battery
+		battery=$(upower -e | grep -m 1 battery)
+		upower -i $battery | awk '/state/ {print $2}'
 	elif command_exists "acpi"; then
-		acpi -b | grep -oi 'discharging' | awk '{print tolower($0)}'
+		acpi -b | awk '{gsub(/,/, ""); print tolower($3); exit}'
 	fi
 }
