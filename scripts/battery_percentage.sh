91b05110cc35863e3f6c3992c9f92916732b26ac
wzy <32936898+Freed-Wu@users.noreply.github.com>
2025-06-10 20:14:31 +0800

    Fix termux (#114)

--- scripts/battery_percentage.sh
+++ scripts/battery_percentage.sh
@@ -6,11 +6,7 @@ source "$CURRENT_DIR/helpers.sh"
 
 print_battery_percentage() {
 	# percentage displayed in the 2nd field of the 2nd row
-	if is_wsl; then
-		local battery
-		battery=$(find /sys/class/power_supply/*/capacity | tail -n1)
-		cat "$battery"
-	elif command_exists "pmset"; then
+	if command_exists "pmset"; then
 		pmset -g batt | grep -o "[0-9]\{1,3\}%"
 	elif command_exists "acpi"; then
 		acpi -b | grep -m 1 -Eo "[0-9]+%"
@@ -36,6 +32,12 @@ print_battery_percentage() {
 		termux-battery-status | jq -r '.percentage' | awk '{printf("%d%%", $1)}'
 	elif command_exists "apm"; then
 		apm -l
+	elif command_exists "termux-battery-status" "jq"; then
+		termux-battery-status | jq -er '"\(.percentage)%"'
+	elif is_wsl; then
+		local battery
+		battery=$(find /sys/class/power_supply/*/capacity | tail -n1)
+		cat "$battery"
 	fi
 }
 
--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -32,16 +32,14 @@ is_wsl() {
 }
 
 command_exists() {
-	local command="$1"
-	type "$command" >/dev/null 2>&1
+	local command
+	for command; do
+		type "$command" >/dev/null 2>&1 || return $?
+	done
 }
 
 battery_status() {
-	if is_wsl; then
-		local battery
-		battery=$(find /sys/class/power_supply/*/status | tail -n1)
-		awk '{print tolower($0);}' "$battery"
-	elif command_exists "pmset"; then
+	if command_exists "pmset"; then
 		pmset -g batt | awk -F '; *' 'NR==2 { print $2 }'
 	elif command_exists "acpi"; then
 		acpi -b | awk '{gsub(/,/, ""); print tolower($3); exit}'
@@ -49,8 +47,6 @@ battery_status() {
 		local battery
 		battery=$(upower -e | grep -E 'battery|DisplayDevice'| tail -n1)
 		upower -i $battery | awk '/state/ {print $2}'
-	elif command_exists "termux-battery-status"; then
-		termux-battery-status | jq -r '.status' | awk '{printf("%s%", tolower($1))}'
 	elif command_exists "apm"; then
 		local battery
 		battery=$(apm -a)
@@ -59,5 +55,11 @@ battery_status() {
 		elif [ $battery -eq 1 ]; then
 			echo "charging"
 		fi
+	elif command_exists "termux-battery-status" "jq"; then
+		termux-battery-status | jq -er '.status | ascii_downcase'
+	elif is_wsl; then
+		local battery
+		battery=$(find /sys/class/power_supply/*/status | tail -n1)
+		awk '{print tolower($0);}' "$battery"
 	fi
 }
